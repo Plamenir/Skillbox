c8d7afc2e480c7a584d72010c4ab511c08a8edad
Plamenir <m.leonid@nxt.ru>
2021-02-05 00:16:13 +0500

    Task3 done

--- Lesson19/Task3/main.cpp
+++ Lesson19/Task3/main.cpp
@@ -1,8 +1,38 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <sstream>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+double action(std::string input, char act) {
+  std::stringstream buffer;
+  std::string number1, number2;
+  buffer << input;
+  buffer >> number1 >> number2;
+  switch (act) {
+    case '+':
+      return stod(number1) + stod(number2);
+    case '-':
+      return stod(number1) - stod(number2);
+    case '*':
+      return stod(number1) * stod(number2);
+    case '/':
+      return stod(number1) / stod(number2);
+  }
+}
 
-    return a.exec();
+int main() {
+  std::string input;
+  char act;
+  std::cout << "Что нужно посчитать?\n";
+  std::cin >> input;
+
+  for (int i = 0; i < input.length(); ++i) {
+    if (input[i] == '+' || input[i] == '-' || input[i] == '*' ||
+        input[i] == '/') {
+      act = input[i];
+      input[i] = ' ';
+      break;
+    }
+  }
+  double result;
+  result = action(input, act);
+  std::cout << "Результат: " << result << "\n";
 }
