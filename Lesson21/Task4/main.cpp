db0a36d45535c05c37ceca1c2a7b2d1fd355f98c
Plamenir <m.leonid@nxt.ru>
2021-02-10 01:31:16 +0500

    fix

--- Lesson21/Task4/main.cpp
+++ Lesson21/Task4/main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
-void evenCnt(long long number, int &ans) {
-  while (number != 0) {
-    if ((number % 10) % 2 == 0) ++ans;
-    number /= 10;
-  }
-}
+void evenCnt(long long number, int &cnt);
 
 int main() {
   long long number;
@@ -14,4 +9,9 @@ int main() {
   std::cin >> number;
   evenCnt(number, ans);
   std::cout << ans << "\n";
+}
+void evenCnt(long long number, int &cnt) {
+  if (number == 0) return;
+  cnt++;
+  evenCnt(number / 10, cnt);
 }
