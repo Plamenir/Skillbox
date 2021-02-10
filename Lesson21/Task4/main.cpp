471f50b23b2ddb89ded7ab41fb04bf05c6accbd9
Plamenir <m.leonid@nxt.ru>
2021-02-10 21:31:36 +0500

    fix

--- Lesson21/Task4/main.cpp
+++ Lesson21/Task4/main.cpp
@@ -7,14 +7,10 @@ int main() {
   int ans = 0;
   std::cout << "Введите число: ";
   std::cin >> number;
-  if (number == 0)
-    ans = 1;
-  else
-    evenCnt(number, ans);
+  evenCnt(number, ans);
   std::cout << ans << "\n";
 }
 void evenCnt(long long number, int &cnt) {
-  if (number == 0) return;
   if ((number % 10) % 2 == 0) cnt++;
-  evenCnt(number / 10, cnt);
+  if (number / 10) evenCnt(number / 10, cnt);
 }
